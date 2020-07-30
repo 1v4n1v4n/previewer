2b634702e94f35291a39e9091135651d1c630cf8
Ivan <filipovmajstor@gmail.com>
2020-07-30 15:18:12 -0500

    Updates

---
+++ precache-manifest.b5ef23d7d710f87e8517e5b71500d32d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e905b11df34f9257bfc76bd276b6c307",
+    "url": "/previewer/index.html"
+  },
+  {
+    "revision": "7fe9ecbbf209a771cdd5",
+    "url": "/previewer/static/css/main.cca75605.chunk.css"
+  },
+  {
+    "revision": "f10ff83e9e8976d100aa",
+    "url": "/previewer/static/js/2.9f645172.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/previewer/static/js/2.9f645172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe9ecbbf209a771cdd5",
+    "url": "/previewer/static/js/main.e206a422.chunk.js"
+  },
+  {
+    "revision": "bb3941f3f29f4ce2a821",
+    "url": "/previewer/static/js/runtime-main.f5f5cca6.js"
+  }
+]);
